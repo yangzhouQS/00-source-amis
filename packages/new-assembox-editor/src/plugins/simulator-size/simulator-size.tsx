f3f08098775ab38be5103e89701672305836d172
yangzhou <yearrow@qq.com>
2026-08-05 13:08:48 +0800

    feat(simulator-size): 美化模拟器尺寸选择组件样式
    - 引入 useAssemNamespace 统一命名空间管理
    - 添加独立样式文件 simulator-size-style.less
    - 为根容器添加命名空间生成的 class 名以便样式作用
    - 调整图标右边距样式由 2px 改为 8px，提高视觉间距
    - 优化代码结构，保持组件层次清晰

--- packages/new-assembox-editor/src/plugins/simulator-size/simulator-size.tsx
+++ packages/new-assembox-editor/src/plugins/simulator-size/simulator-size.tsx
@@ -6,6 +6,10 @@ import { Cellphone, Iphone, Monitor } from "@element-plus/icons-vue";
  */
 import { defineComponent, PropType } from "vue";
 import { DEVICE_PRESETS } from "../../core/store";
+import { useAssemNamespace } from "../../hooks/use-assem-namespace";
+import "./simulator-size-style.less";
+
+const ns = useAssemNamespace("simulator-size");
 
 const ICON_MAP: Record<string, any> = {
   default: Monitor,
@@ -22,32 +26,34 @@ export const SimulatorSize = defineComponent({
     return () => {
       const current = props.editor.store.state.device.key;
       return (
-        <el-radio-group
-          modelValue={current}
-          size="small"
-          onUpdate:modelValue={(val: any) => {
-            const preset = DEVICE_PRESETS.find(d => d.key === val);
-            if (preset) {
-              props.editor.store.setDevice(preset);
-            }
-          }}
-        >
-          {DEVICE_PRESETS.map((d) => {
-            const Icon = ICON_MAP[d.key];
-            return (
-              <el-radio-button key={d.key} value={d.key}>
-                {Icon
-                  ? (
-                      <el-icon style="margin-right:2px">
-                        <Icon />
-                      </el-icon>
-                    )
-                  : null}
-                {d.label}
-              </el-radio-button>
-            );
-          })}
-        </el-radio-group>
+        <div class={ns.b()}>
+          <el-radio-group
+            modelValue={current}
+            size="small"
+            onUpdate:modelValue={(val: any) => {
+              const preset = DEVICE_PRESETS.find(d => d.key === val);
+              if (preset) {
+                props.editor.store.setDevice(preset);
+              }
+            }}
+          >
+            {DEVICE_PRESETS.map((d) => {
+              const Icon = ICON_MAP[d.key];
+              return (
+                <el-radio-button key={d.key} value={d.key}>
+                  {Icon
+                    ? (
+                        <el-icon style={{ marginRight: "8px" }}>
+                          <Icon />
+                        </el-icon>
+                      )
+                    : null}
+                  {d.label}
+                </el-radio-button>
+              );
+            })}
+          </el-radio-group>
+        </div>
       );
     };
   },
